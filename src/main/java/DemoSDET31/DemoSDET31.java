954dac9c01b1b2fb40edb707ebaa28ea0c699019
singh <singh@Howtozap>
2022-03-10 16:24:47 +0530

    User1 First commit

---
+++ src/main/java/DemoSDET31/DemoSDET31.java
@@ -0,0 +1,13 @@
+package DemoSDET31;
+
+import org.testng.annotations.Test;
+
+public class DemoSDET31 {
+	
+	@Test
+	public void Demo()
+	{
+		System.out.println("User1 - account created");
+	}
+
+}
